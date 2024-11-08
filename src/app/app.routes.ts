d68d26f7ea5987f8494ce0d59e798615d1835c92
vladboisa <vladboisa@gmail.com>
2024-11-08 19:26:29 +0100

    feat: add routes to garage and winners

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CarsListComponent } from './garage/containers/cars-list/cars-list.component';
+import { WinnersListComponent } from './winners/containers/winners-list/winners-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'garage',
+    pathMatch: 'full',
+  },
+  { path: 'garage', component: CarsListComponent },
+  { path: 'winners', component: WinnersListComponent },
+  { path: '**', redirectTo: 'garage' },
+];
