18c18c12da6d939b564b21c3c8d01601603861b8
vladboisa <vladboisa@gmail.com>
2024-11-08 19:04:36 +0100

    feat: add cars-list container

---
+++ src/app/garage/containers/cars-list/cars-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-list',
+  standalone: true,
+  imports: [],
+  template: ` <p>cars-list works!</p> `,
+  styleUrl: './cars-list.component.scss',
+})
+export class CarsListComponent {}
