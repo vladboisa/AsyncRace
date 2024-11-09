411f0f7726402eb10b85f6a6c3686f7b53564a18
vladboisa <vladboisa@gmail.com>
2024-11-09 11:23:08 +0100

    feat: add cars card component

---
+++ src/app/garage/components/cars-card/cars-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-card',
+  standalone: true,
+  imports: [],
+  template: ` <p>cars-card works!</p> `,
+  styleUrl: './cars-card.component.scss',
+})
+export class CarsCardComponent {}
