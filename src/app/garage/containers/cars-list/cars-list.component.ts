9d281a295b1b03d2412f932c3e41f1cd4d08e640
vladboisa <vladboisa@gmail.com>
2024-11-09 20:36:22 +0100

    feat: add cars buttons & wrapper to view

--- src/app/garage/containers/cars-list/cars-list.component.ts
+++ src/app/garage/containers/cars-list/cars-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CarsWrapperComponent } from '../../components/cars-wrapper/cars-wrapper.component';
+import { CarsButtonsComponent } from '../../components/cars-buttons/cars-buttons.component';
 
 @Component({
   selector: 'app-cars-list',
   standalone: true,
-  imports: [],
-  template: ` <p>cars-list works!</p> `,
+  imports: [CarsWrapperComponent, CarsButtonsComponent],
+  template: `
+    <main>
+      <app-cars-buttons />
+      <app-cars-wrapper />
+    </main>
+  `,
   styleUrl: './cars-list.component.scss',
 })
 export class CarsListComponent {}
