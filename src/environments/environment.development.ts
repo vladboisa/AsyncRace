e1bf3c8d00c71e9695052db2b3927fa9d9d31328
vladboisa <vladboisa@gmail.com>
2024-11-12 19:50:49 +0100

    feat: add apiGarage in evnironment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   api: 'http://127.0.0.1:3000',
+  apiGarage: 'http://127.0.0.1:3000/garage',
+  apiWinners: 'http://127.0.0.1:3000/winners',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   api: 'https://responsible-dazzling-emery.glitch.me/',
+  apiGarage: 'https://responsible-dazzling-emery.glitch.me/garage',
+  apiWinners: 'https://responsible-dazzling-emery.glitch.me/winners',
 };
