771f6b26ae0b1a1275544e8a5a9532d34efad020
vladboisa <vladboisa@gmail.com>
2024-11-16 14:11:46 +0100

    feat: create randomCars service

---
+++ src/app/services/random-cars.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Car } from '../../models/api.models';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RandomCarsService {
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private readonly CAR_BRANDS = [
+    'Tesla',
+    'Ford',
+    'Chevrolet',
+    'BMW',
+    'Audi',
+    'Toyota',
+    'Honda',
+    'Mercedes',
+    'Nissan',
+    'Volkswagen',
+  ];
+  private readonly CAR_MODELS = [
+    'Model S',
+    'Mustang',
+    'Corvette',
+    'X5',
+    'A4',
+    'Camry',
+    'Civic',
+    'C-Class',
+    'Altima',
+    'Golf',
+  ];
+
+  constructor() {}
+  private generateRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  private generateRandomCarName(): string {
+    const brand = this.CAR_BRANDS[Math.floor(Math.random() * this.CAR_BRANDS.length)];
+    const model = this.CAR_MODELS[Math.floor(Math.random() * this.CAR_MODELS.length)];
+    return `${brand} ${model}`;
+  }
+  createArrayCars(amount = 100) {
+    const randomCars: Car[] = Array.from({ length: amount }).map(() => ({
+      name: this.generateRandomCarName(),
+      color: this.generateRandomColor(),
+    }));
+    return randomCars;
+  }
+}
