62ad94ffdd0c75ff63b4b816b8b2facc1dfda388
vladboisa <vladboisa@gmail.com>
2024-11-18 19:31:44 +0100

    feat: add animation service

---
+++ src/app/services/feature/animation.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimationService {
+  private animationFrameId!: number;
+
+  animateCar(duration: number, onProgress: (progress: number) => void, onComplete: () => void) {
+    const startTime = performance.now();
+
+    const animate = (time: number) => {
+      const elapsedTime = (time - startTime) / 1000;
+      const progress = Math.min(elapsedTime / duration, 1);
+      onProgress(progress);
+      if (progress < 1) {
+        this.animationFrameId = requestAnimationFrame(animate);
+      } else {
+        onComplete();
+      }
+    };
+    this.animationFrameId = requestAnimationFrame(animate);
+  }
+  cancelAnimation() {
+    cancelAnimationFrame(this.animationFrameId);
+  }
+}
