9cd4a5125a4cb4bf84ba2d6b9c969c4a08c745c8
Vlad Boisa <60569670+vladboisa@users.noreply.github.com>
2024-11-20 12:13:50 +0000

    feat: implement winner view logic

--- src/app/winners/components/winners-list/winners-list.component.ts
+++ src/app/winners/components/winners-list/winners-list.component.ts
@@ -1,11 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { WinnersCardComponent } from '../../containers/winners-card/winners-card.component';
+import { MatTableModule } from '@angular/material/table';
+import { WinnersService } from '../../../services/core/winners/winners.service';
+import { AsyncPipe } from '@angular/common';
+import { CarsService } from '../../../services/core/cars/cars.service';
 
 @Component({
   selector: 'app-winners-list',
   standalone: true,
-  imports: [],
+  imports: [WinnersCardComponent, MatTableModule, AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: ` <p>winners-list works!</p> `,
+  template: `
+    <table mat-table [dataSource]="(winners$ | async) || []">
+      <ng-container matColumnDef="id">
+        <th mat-header-cell *matHeaderCellDef>Id</th>
+        <td mat-cell *matCellDef="let element">{{ element.id }}</td>
+      </ng-container>
+      <ng-container matColumnDef="name">
+        <th mat-header-cell *matHeaderCellDef>Name</th>
+        <td mat-cell *matCellDef="let element">{{ element.name }}</td>
+      </ng-container>
+      <ng-container matColumnDef="wins">
+        <th mat-header-cell *matHeaderCellDef>Wins</th>
+        <td mat-cell *matCellDef="let element">{{ element.wins }}</td>
+      </ng-container>
+      <ng-container matColumnDef="time">
+        <th mat-header-cell *matHeaderCellDef>Time</th>
+        <td mat-cell *matCellDef="let element">{{ element.time / TIME_CONVERT_RATIO_MS }} sec.</td>
+      </ng-container>
+
+      <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+      <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+    </table>
+
+    <app-winners-card />
+  `,
   styleUrl: './winners-list.component.scss',
 })
-export class WinnersListComponent {}
+export class WinnersListComponent implements OnInit {
+  readonly TIME_CONVERT_RATIO_MS = 1000;
+  private readonly winnersService = inject(WinnersService);
+  private readonly carsService = inject(CarsService);
+  protected winners$ = this.winnersService.winners$;
+  displayedColumns: string[] = ['id', 'name', 'wins', 'time'];
+  ngOnInit() {
+    this.winnersService.readAllWinners().subscribe(console.log);
+  }
+}
