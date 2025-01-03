5902011fa5a6696b1f48cd390ad769d30e2cfddf
vladboisa <vladboisa@gmail.com>
2025-01-03 21:09:52 +0100

    chore: write new line

--- src/app/interceptors/global-error-handler.interceptor.ts
+++ src/app/interceptors/global-error-handler.interceptor.ts
@@ -5,5 +5,6 @@ import { ErrorsService } from '../services/errors.service';
 
 export const globalErrorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
   const errorsHandler = inject(ErrorsService);
+
   return next(req).pipe(retry({ count: 2, delay: 5000 }), catchError(errorsHandler.handleError));
 };
