63001306a67db4f6744965ee9dd633ce8136cfdd
vladboisa <vladboisa@gmail.com>
2025-04-03 19:26:56 +0100

    Auto-generated commit

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  api: 'http://127.0.0.1:3000',
-  apiGarage: 'http://127.0.0.1:3000/garage',
-  apiWinners: 'http://127.0.0.1:3000/winners',
-  apiEngine: 'http://127.0.0.1:3000/engine',
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'https://achieved-deciduous-kale.glitch.me/',
-  apiGarage: 'https://achieved-deciduous-kale.glitch.me/garage',
-  apiWinners: 'https://achieved-deciduous-kale.glitch.me/winners',
-  apiEngine: 'https://achieved-deciduous-kale.glitch.me/engine',
-};
